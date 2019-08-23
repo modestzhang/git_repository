c3e6e9fee3e69672bc0c7ad08022d4b5a0292d49
zhangtl <zhangtl1994@163.com>
2019-08-23 22:51:46 +0800

    0823

---
+++ learn_python/chapter_7.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time      : 2019/8/23 22:27
+# @Author    : modestzhang
+# @project   : learn_python
+# @File      : chapter_7.py
+# @Copyright : Manjusaka
+
+
+# 用户输入
+# name = input("Please enter your name:")
+# print(name)
+#
+# age = int(input())
+# print(age)
+
+current_number = 1
+while current_number <= 5:
+    print(current_number)
+    current_number += 1
+    if current_number == 2:
+        continue
+    if current_number == 3:
+        break
